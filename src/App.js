bddf923d49d40155442a14292a7d1251b9016532
Niilo Latva-Pukkila <niilo.latva-pukkila@kesko.fi>
2020-03-06 13:38:11 +0200

    add drawing canvas

--- src/App.js
+++ src/App.js
@@ -1,73 +1,13 @@
-import React, { useEffect, useReducer } from 'react';
+import React from "react";
+import CanvasDraw from "react-canvas-draw";
 
-import API, { graphqlOperation } from '@aws-amplify/api';
-import PubSub from '@aws-amplify/pubsub';
-
-import { createTodo } from './graphql/mutations';
-import { listTodos } from './graphql/queries';
-import { onCreateTodo } from './graphql/subscriptions';
-
-import awsconfig from './aws-exports';
-import './App.css';
-
-API.configure(awsconfig);
-PubSub.configure(awsconfig);
-
-// Action Types
-const QUERY = 'QUERY';
-const SUBSCRIPTION = 'SUBSCRIPTION';
-
-const initialState = {
-  todos: [],
-};
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case QUERY:
-      return { ...state, todos: action.todos };
-    case SUBSCRIPTION:
-      return { ...state, todos: [...state.todos, action.todo] }
-    default:
-      return state;
-  }
-};
-
-async function createNewTodo() {
-  const todo = { name: "Use AWS AppSync", description: "RealTime and Offline" };
-  await API.graphql(graphqlOperation(createTodo, { input: todo }));
-}
-
-function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  useEffect(() => {
-    async function getData() {
-      const todoData = await API.graphql(graphqlOperation(listTodos));
-      dispatch({ type: QUERY, todos: todoData.data.listTodos.items });
-    }
-    getData();
-
-    const subscription = API.graphql(graphqlOperation(onCreateTodo)).subscribe({
-      next: (eventData) => {
-        const todo = eventData.value.data.onCreateTodo;
-        dispatch({ type: SUBSCRIPTION, todo });
-      }
-    });
-
-    return () => subscription.unsubscribe();
-  }, []);
-
+const App = () => {
   return (
-    <div className="App">
-      <button onClick={createNewTodo}>Add Todo</button>
-      <div>
-        {state.todos.length > 0 ?
-          state.todos.map((todo) => <p key={todo.id}>{todo.name} : {todo.description}</p>) :
-          <p>Add some todos!</p>
-        }
-      </div>
+    <div>
+    <h1>Welcome to Niilo's Drawing Game!</h1>
+    <CanvasDraw />
     </div>
-  );
-}
+  )
 
-export default App;
+}
+export default App 
